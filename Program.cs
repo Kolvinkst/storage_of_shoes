2c1c8bc1a7f5936ee36ce539036637b0d6a0fea3
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-18 07:40:30 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_of_shoes
+{
+    internal class Program
+    {
+        static ShoeCloset shoeCloset = new ShoeCloset();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                shoeCloset.PrintShoes();
+                Console.Write("\nPress 'a' to add or 'r' to remove a shoe: ");
+                char key = Console.ReadKey().KeyChar;
+
+                switch (key)
+                {
+                    case 'a':
+                    case 'A':
+                        shoeCloset.AddShoe();
+                        break;
+                    case 'r':
+                    case 'R':
+                        shoeCloset.RemoveShoe();
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+    }
+}
